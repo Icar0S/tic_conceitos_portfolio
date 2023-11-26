8d76488b0c2e6de486f73f93f17acac7dbd6c56e
icar0s <icarooliveira_23@hotmail.com>
2023-11-26 12:34:22 -0300

    adding studies variables types

---
+++ treino-kotlin/src/main/kotlin/variables-types.kt
@@ -0,0 +1,22 @@
+fun main(){
+    var testM: Int = 11//mutate
+    val testIm: Int = 23//immutable
+
+    var testFloat: Float = 123.45f
+    var testChar: Char = 'F'
+    var testStr: String = "Icaro"
+    var testShort: Short = 100
+    var testByteBin: Byte = 0b1010 //bin
+    var testByteHex: Byte = 0xf //hex
+    var testUint: UInt = 212u
+
+    println(testM)
+    println(testIm)
+    println(testFloat)
+    println(testChar)
+    println(testStr)
+    println(testShort)
+    println(testByteBin)
+    println(testByteHex)
+    println(testUint)
+}
