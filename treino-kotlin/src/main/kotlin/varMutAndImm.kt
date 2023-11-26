e5e62f06a05aaa82160bfc7c9e0fbd862095e954
icar0s <icarooliveira_23@hotmail.com>
2023-11-26 13:15:08 -0300

    add studing variables types

---
+++ treino-kotlin/src/main/kotlin/varMutAndImm.kt
@@ -0,0 +1,10 @@
+fun main(){
+
+    val num:Int = 23
+    var name:String = "test"
+
+    //num += 3
+    name += " passed"
+    println( num )
+    println(  name )
+}
--- treino-kotlin/src/main/kotlin/variables-types.kt
+++ treino-kotlin/src/main/kotlin/variables-types.kt
@@ -1,7 +1,6 @@
 fun main(){
     var testM: Int = 11//mutate
     val testIm: Int = 23//immutable
-
     var testFloat: Float = 123.45f
     var testChar: Char = 'F'
     var testStr: String = "Icaro"
@@ -9,7 +8,6 @@ fun main(){
     var testByteBin: Byte = 0b1010 //bin
     var testByteHex: Byte = 0xf //hex
     var testUint: UInt = 212u
-
     println(testM)
     println(testIm)
     println(testFloat)
@@ -19,4 +17,10 @@ fun main(){
     println(testByteBin)
     println(testByteHex)
     println(testUint)
+    println("=========================")
+    println("CAPACIDADE DE CADA TIPO")
+    println("ULong max: ${ULong.MAX_VALUE} ULong min: ${ULong.MIN_VALUE}")
+    println("UInt max: ${UInt.MAX_VALUE} UInt min: ${UInt.MIN_VALUE}")
+    println("Short max: ${Short.MAX_VALUE} Short min: ${Short.MIN_VALUE}")
+
 }
