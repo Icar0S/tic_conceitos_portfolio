2f856c52166e6ffaa4ae5cd60590fef293d3fd4c
icar0s <icarooliveira_23@hotmail.com>
2023-11-26 16:11:28 -0300

    add studying functions types

---
+++ treino-kotlin/src/main/kotlin/FunctionBase.kt
@@ -0,0 +1,42 @@
+//fun Imprimir(){
+//    println("test")
+//}
+//fun Soma( a:Int, b:Int):Int{
+//    return a+b
+//}
+
+fun casa(){
+    println("Entrando na casa...")
+    sala()
+}
+fun sala(){
+    println("Entrando na sala...")
+    corredor()
+}
+fun corredor(){
+    println("Entrando no corredor...")
+    quarto()
+}
+fun quarto(){
+    println("Entrando no quarto ...")
+    mesa()
+}
+fun mesa(){
+    println("Acessando a mesa de trabalho ...")
+    computador()
+}
+fun computador(){
+    println("Abrindo o computador ...")
+    hd()
+}
+fun hd(){
+    println("Pegando o hd externo ...")
+}
+
+fun main(){
+    casa()
+
+//    Imprimir()
+//    var x=10; var y=23
+//    println(Soma(x, y));
+}
