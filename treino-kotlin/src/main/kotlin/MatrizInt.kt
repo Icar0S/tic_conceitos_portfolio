710072765068d43e36ccb857b3003f9cfd724446
icar0s <icarooliveira_23@hotmail.com>
2023-11-26 16:58:56 -0300

    add studying functions matrices types

---
+++ treino-kotlin/src/main/kotlin/MatrizInt.kt
@@ -0,0 +1,29 @@
+fun main(){
+    val lin = 3
+    val col = 6
+    val matriz: Array<Array<Int>> = Array(lin) { Array(col) {0} }
+    var dig = -1
+
+    for(i in 0 until lin){
+        for(j in 0 until col){
+            print("${matriz[i][j]} ")
+        }
+        println()
+    }
+    println("===== IMPRESSÃO DA MATRIZ =====")
+    for(i in 0 until lin){
+        for(j in 0 until col){
+            matriz[i][j] = ++dig
+            print("${matriz[i][j]}  ")
+        }
+        println()
+    }
+    println("===== IMPRESSÃO 2 DA MATRIZ =====")
+    for( lin in matriz ){
+        for( valor in lin ){
+            print("${valor} ")
+        }
+        println()
+    }
+
+}
